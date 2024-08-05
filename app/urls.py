bb34f799a700a117d11c754f0cbfa876ba99fab7
Manoel Cândido <candidomanoel78@gmail.com>
2024-08-05 07:26:11 -0300

    creating login url

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from . import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('login/', auth_views.LoginView.as_view() ,name='login'),
     path('', include('brands.urls')),
     path('', include('categories.urls')),
     path('', include('suppliers.urls')),
