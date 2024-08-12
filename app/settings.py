e5ae5bd1e7027826f756c7fba245ce0fdc7381aa
Manoel Cândido <candidomanoel78@gmail.com>
2024-08-12 07:32:40 -0300

    implementing logout

--- app/settings.py
+++ app/settings.py
@@ -48,6 +48,8 @@ INSTALLED_APPS = [
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = '/'
 
+LOGOUT_REDIRECT_URL = '/login/'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
