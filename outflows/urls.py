f165105f48c5bc40f30b0880281287066883b089
Manoel Cândido <candidomanoel78@gmail.com>
2024-09-23 08:53:55 -0300

    creating outflows API

--- outflows/urls.py
+++ outflows/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('outflows/list/', views.OutflowListView.as_view(),  name='outflow-list'),
     path('outflows/create/', views.OutflowCreateView.as_view(), name='outflow-create'),
     path('outflows/<int:pk>/detail/', views.OutflowDetailView.as_view(), name='outflow-detail'),
+
+    path('api/v1/outflows/', views.OutflowCreateListAPIView.as_view(), name='outflow-create-list-api'),
+    path('api/v1/outflows/<int:pk>/', views.OutflowRetriveAPIView.as_view(), name='outflow-detail-api'),
 ]
