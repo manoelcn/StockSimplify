28e898dc2d427870866345ecd68fa9491edb4d9d
Manoel Cândido <candidomanoel78@gmail.com>
2024-09-23 07:43:24 -0300

    creating brands API

--- brands/urls.py
+++ brands/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('brands/<int:pk>/detail/', views.BrandDetailView.as_view(), name='brand-detail'),
     path('brands/<int:pk>/update/', views.BrandUpdateView.as_view(), name='brand-update'),
     path('brands/<int:pk>/delete/', views.BrandDeleteView.as_view(), name='brand-delete'),
+
+    path('api/v1/brands/', views.BrandCreateListAPIView.as_view(), name='brand-create-list-api'),
+    path('api/v1/brands/<int:pk>/', views.BrandRetriveUpdateDestroyAPIView.as_view(), name='brand-detail-api')
 ]
