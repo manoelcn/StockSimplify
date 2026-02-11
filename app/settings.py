7d8b3c068ec97e27f185ff4d40470916dc17105e
Manoel <candidomanoel78@gmail.com>
2026-02-11 18:12:56 -0300

    removing outdated configuration from static files

--- app/settings.py
+++ app/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
     'brands',
